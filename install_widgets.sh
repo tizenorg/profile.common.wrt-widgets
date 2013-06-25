12cf9b787edb1ee950cf3bfb1b149bedbef9fdb1
Baptiste DURAND <baptiste.durand@eurogiciel.fr>
2013-06-25 13:51:36 +0200

    Enable the WA for icon creation

--- install_widgets.sh
+++ install_widgets.sh
@@ -25,30 +25,30 @@ chmod -R a+rw /opt/dbspace/
 
 wrt-launcher --list |
 awk 'NR>2{print $2, $5, $6}' |
-#while read name packid appid
-#do
-#    bin=$repo/$packid/bin/$appid
-#    for x in $repo/$packid/res/wgt/*[iI][cC][oO][nN]*; do
-#	if [[ -f $x ]]; then
-#	    res=$(file -b $x|cut -d , -f 2|tr -d ' '|egrep '[0-9]+x[0-9]+')
-#	    if [[ -n "$res" ]]; then
-#		diric=/usr/share/icons/hicolor/$res/apps
-#		[[ -d $diric ]] || mkdir -p $diric
-#		cp $x $diric/$name.png
-#	    fi
-#	fi
-#   done
-#    desk=/usr/share/applications/$name.desktop
-#    cat << EOC > $desk
-#[Desktop Entry]
-#Type=Application
-#Name=$name
-#Exec=$bin
-#Icon=$name
-#Terminal=false
-#Categories=WRT;Game
-#EOC
-#done
+while read name packid appid
+do
+    bin=$repo/$packid/bin/$appid
+    for x in $repo/$packid/res/wgt/*[iI][cC][oO][nN]*; do
+	if [[ -f $x ]]; then
+	    res=$(file -b $x|cut -d , -f 2|tr -d ' '|egrep '[0-9]+x[0-9]+')
+	    if [[ -n "$res" ]]; then
+		diric=/usr/share/icons/hicolor/$res/apps
+		[[ -d $diric ]] || mkdir -p $diric
+		cp $x $diric/$name.png
+	    fi
+	fi
+   done
+    desk=/usr/share/applications/$name.desktop
+    cat << EOC > $desk
+[Desktop Entry]
+Type=Application
+Name=$name
+Exec=$bin
+Icon=$name
+Terminal=false
+Categories=WRT;Game
+EOC
+done
 
 update-desktop-database
 xdg-icon-resource forceupdate
